b2f09e8db9737b4a7f6ce87a82e2f2d2c60c705e
CorwinDev <88144943+CorwinDev@users.noreply.github.com>
2022-02-04 16:55:35 +0100

    0.0.2
    0.0.2
    First base of the next generation CorwinDev.nl :)

---
+++ index.php
@@ -0,0 +1,18 @@
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/contact.php">Contact</a></li>
+    <li><a href="/projects.php">Projects</a></li>
+</ul>
+
+
+<body>
+    <br>
+    <div class="headd">
+        <br><br><br><br><br>
+        <h1>CorwinDev</h1>
+    </div>
+</body>
