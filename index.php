21b3de109262f09f8066130eeb71e6f8147a8487
CorwinDev <88144943+CorwinDev@users.noreply.github.com>
2022-02-07 16:15:16 +0100

    Added nl/en function

--- index.php
+++ index.php
@@ -1,100 +1,208 @@
 <?php
+if (!$_GET["language"] || $_GET["language"] === "en") {
+?>
+
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+        <script>
+            var words = ['HTML', 'PHP', 'JS', 'JAVA'],
+                part,
+                i = 0,
+                offset = 0,
+                len = words.length,
+                forwards = true,
+                skip_count = 0,
+                skip_delay = 15,
+                speed = 70;
+            var wordflick = function() {
+                setInterval(function() {
+                    if (forwards) {
+                        if (offset >= words[i].length) {
+                            ++skip_count;
+                            if (skip_count == skip_delay) {
+                                forwards = false;
+                                skip_count = 0;
+                            }
+                        }
+                    } else {
+                        if (offset == 0) {
+                            forwards = true;
+                            i++;
+                            offset = 0;
+                            if (i >= len) {
+                                i = 0;
+                            }
+                        }
+                    }
+                    part = words[i].substr(0, offset);
+                    if (skip_count == 0) {
+                        if (forwards) {
+                            offset++;
+                        } else {
+                            offset--;
+                        }
+                    }
+                    $('.word').text(part);
+                }, speed);
+            };
+
+            $(document).ready(function() {
+                wordflick();
+            });
+        </script>
+    </head>
+
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/contact.php">Contact</a></li>
+        <li><a href="/projects.php">Projects</a></li>
+
+    </ul>
+
+
+    <body>
+        <br>
+        <div class="headd">
+            <br>
+
+            <h1 class="animate-charcter"> Corwin</h1>
+
+
+            <p class="underdev">Developer in many languages!</p>
+            <br>
+            <div class="ocean">
+                <div class="wave"></div>
+                <div class="wave"></div>
+                <div class="wave"></div>
+            </div>
+        </div>
+        <br>
+        <div class="overmij">
+            <h2 class="overmijh1">About Corwin</h2>
+            <p class="overmijtext">
+                I'm a programmer from the Netherlands <br> and have about 4 years experience in development!
+            </p>
+            <br>
+
+        </div>
+        <br>
+
+    </body>
+    <div class="languages">
+        <br>
+        <div class="languagesiu">Languages I understand:</div>
+        <div class="word">
+        </div>
+        <br>
+    </div>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+<?php
+} else {
 
 ?>
 
-<head>
-    <link rel="stylesheet" href="style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-    <script>
-        var words = ['HTML', 'PHP', 'JS', 'JAVA'],
-            part,
-            i = 0,
-            offset = 0,
-            len = words.length,
-            forwards = true,
-            skip_count = 0,
-            skip_delay = 15,
-            speed = 70;
-        var wordflick = function() {
-            setInterval(function() {
-                if (forwards) {
-                    if (offset >= words[i].length) {
-                        ++skip_count;
-                        if (skip_count == skip_delay) {
-                            forwards = false;
-                            skip_count = 0;
-                        }
-                    }
-                } else {
-                    if (offset == 0) {
-                        forwards = true;
-                        i++;
-                        offset = 0;
-                        if (i >= len) {
-                            i = 0;
-                        }
-                    }
-                }
-                part = words[i].substr(0, offset);
-                if (skip_count == 0) {
+        <script>
+            var words = ['HTML', 'PHP', 'JS', 'JAVA'],
+                part,
+                i = 0,
+                offset = 0,
+                len = words.length,
+                forwards = true,
+                skip_count = 0,
+                skip_delay = 15,
+                speed = 70;
+            var wordflick = function() {
+                setInterval(function() {
                     if (forwards) {
-                        offset++;
+                        if (offset >= words[i].length) {
+                            ++skip_count;
+                            if (skip_count == skip_delay) {
+                                forwards = false;
+                                skip_count = 0;
+                            }
+                        }
                     } else {
-                        offset--;
+                        if (offset == 0) {
+                            forwards = true;
+                            i++;
+                            offset = 0;
+                            if (i >= len) {
+                                i = 0;
+                            }
+                        }
                     }
-                }
-                $('.word').text(part);
-            }, speed);
-        };
+                    part = words[i].substr(0, offset);
+                    if (skip_count == 0) {
+                        if (forwards) {
+                            offset++;
+                        } else {
+                            offset--;
+                        }
+                    }
+                    $('.word').text(part);
+                }, speed);
+            };
 
-        $(document).ready(function() {
-            wordflick();
-        });
-    </script>
-</head>
+            $(document).ready(function() {
+                wordflick();
+            });
+        </script>
+    </head>
 
-<ul>
-    <li><a href="/">Home</a></li>
-    <li><a href="/contact.php">Contact</a></li>
-    <li><a href="/projects.php">Projects</a></li>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/contact.php">Contact</a></li>
+        <li><a href="/projects.php">Projects</a></li>
+        <label class="switch">
+            <input type="checkbox">
+            <span class="slider round"></span>
+        </label>
+    </ul>
 
-</ul>
 
-
-<body>
-    <br>
-    <div class="headd">
+    <body>
         <br>
+        <div class="headd">
+            <br>
 
-        <h1 class="animate-charcter"> Corwin</h1>
+            <h1 class="animate-charcter"> Corwin</h1>
 
 
-        <p class="underdev">Developer in many languages!</p>
-        <br>
-        <div class="ocean">
-            <div class="wave"></div>
-            <div class="wave"></div>
-            <div class="wave"></div>
+            <p class="underdev">Developer in veel codeer talen!</p>
+            <br>
+            <div class="ocean">
+                <div class="wave"></div>
+                <div class="wave"></div>
+                <div class="wave"></div>
+            </div>
         </div>
-    </div>
-    <br>
-    <div class="overmij">
-        <h2 class="overmijh1">About Corwin</h2>
-        <p class="overmijtext">
-            I'm a programmer from the Netherlands <br> and have about 4 years experience in development!
-        </p>
+        <br>
+        <div class="overmij">
+            <h2 class="overmijh1">Over Corwin</h2>
+            <p class="overmijtext">
+                Ik ben een programmeur uit Nederland <br> en heb al 4 jaar ervaring!
+            </p>
+            <br>
+
+        </div>
         <br>
 
+    </body>
+    <div class="languages">
+        <br>
+        <div class="languagesiu">Programmeer talen die ik kan:</div>
+        <div class="word">
+        </div>
+        <br>
     </div>
-    <br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
-</body>
-<div class="languages">
-    <br>
-    <div class="languagesiu">Languages I understand:</div>
-    <div class="word">
-    </div>
-    <br>
-</div>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<?php
+}
